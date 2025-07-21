8da1db92c1e515f4f20c94c98845d67041a27c67
Yabaa233 <yabaa233@gmail.com>
2025-07-21 20:12:50 +0900

    Test easy ar SpatialMap

--- Assets/Scripts/Manager/ARManager.cs
+++ Assets/Scripts/Manager/ARManager.cs
@@ -1,48 +1,104 @@
 ﻿using System.Collections;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.XR.ARFoundation;
+//using UnityEngine.SceneManagement;
+//using UnityEngine.XR.ARFoundation;
+using easyar;
+using UnityEngine.UI;
+using System;
 
 namespace Assets.Scripts.Manager
 {
     public class ARManager : singleton<ARManager>
     {
+        //public ARSession session;
+
+        //void Start()　
+        //{
+        //    StartCoroutine(InitAR());
+        //}
+
+        //IEnumerator InitAR()
+        //{
+        //    yield return null;
+        //    yield return null;
+        //    yield return new WaitForEndOfFrame();
+
+        //    if (session != null)
+        //    {
+        //        while (ARSession.state < ARSessionState.SessionInitializing)
+        //        {
+        //            Debug.Log("Waiting for ARSession to initialize... State=" + ARSession.state);
+        //            yield return null;
+        //        }
+
+        //        if (ARSession.state >= ARSessionState.SessionInitializing)
+        //        {
+        //            Debug.Log("Resetting ARSession now, state=" + ARSession.state);
+        //            session.Reset();
+        //        }
+        //    }
+        //}
+
+        //void OnDisable()
+        //{
+        //    if (session != null)
+        //    {
+        //        Debug.Log("Disabling ARSession on ARManager...");
+        //        session.enabled = false;
+        //    }
+        //}
+
+        public SparseSpatialMapController map;
+        public SparseSpatialMapWorkerFrameFilter mapWorker;
         public ARSession session;
 
-        void Start()
+        public Button btnSave;
+
+        private void Start()
         {
-            StartCoroutine(InitAR());
+            btnSave.onClick.AddListener(SaveSpatialMap);
+            btnSave.interactable = false;
+            session.StateChanged += OnSessionStateChanged;
         }
 
-        IEnumerator InitAR()
+        /// <summary>
+        /// Save SpatialMap
+        /// </summary>
+        private void SaveSpatialMap()
         {
-            yield return null;
-            yield return null;
-            yield return new WaitForEndOfFrame();
-
-            if (session != null)
+            mapWorker.BuilderMapController.MapHost += SaveMapHostBack;
+            try
             {
-                while (ARSession.state < ARSessionState.SessionInitializing)
-                {
-                    Debug.Log("Waiting for ARSession to initialize... State=" + ARSession.state);
-                    yield return null;
-                }
+                mapWorker.BuilderMapController.Host("SpatialMap250721", null);
+                Debug.Log("Start Save SpatialMap");
+            }
+            catch (Exception ex)
+            {
+                Debug.Log("Save SpatialMap Error :" + ex.Message);
+            }
 
-                if (ARSession.state >= ARSessionState.SessionInitializing)
-                {
-                    Debug.Log("Resetting ARSession now, state=" + ARSession.state);
-                    session.Reset();
-                }
+        }
+
+        private void SaveMapHostBack(
+            SparseSpatialMapController.SparseSpatialMapInfo mapinfo,
+            bool isSuccess,
+            string error)
+        {
+            if (isSuccess)
+            {
+                PlayerPrefs.SetString("MapID", mapinfo.ID);
+                PlayerPrefs.SetString("MapName", mapinfo.Name);
+                Debug.Log("Save SpatialMap Sucess. ID: " + mapinfo.ID);
+            }
+            else
+            {
+                Debug.Log("Save SpatialMap Error :" + error);
             }
         }
 
-        void OnDisable()
+        private void OnSessionStateChanged(ARSession.SessionState state)
         {
-            if (session != null)
-            {
-                Debug.Log("Disabling ARSession on ARManager...");
-                session.enabled = false;
-            }
+            btnSave.interactable = (state == ARSession.SessionState.Ready || state == ARSession.SessionState.Running);
         }
 
     }
