aa37e79be8feb3a1ddf66e5f4a82fb627321c076
Yabaa233 <yabaa233@gmail.com>
2025-12-20 17:08:16 +0900

    实验用mesh上传

--- Assets/Scripts/BackUp/AR/VFXDissolveCaller.cs
+++ Assets/Scripts/BackUp/AR/VFXDissolveCaller.cs
@@ -1,15 +1,14 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class VFXDissolveCaller : MonoBehaviour
 {
-    // 按钮触发或其他方式调用此函数
     public void FindAndTriggerDissolve()
     {
         VFXDissolveController controller = FindObjectOfType<VFXDissolveController>();
 
         if (controller == null)
         {
-            Debug.LogWarning("Cant find VFXDissolveRuntimeController conp！");
+            Debug.LogWarning("Cant find VFXDissolveRuntimeController Component");
             return;
         }
         
